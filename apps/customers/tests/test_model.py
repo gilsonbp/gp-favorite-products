67a8e79d8e9651a12595bb3374c33ab0cdabd304
Gilson Paulino <gilsonbp@gmail.com>
2020-09-19 11:51:39 -0300

    Adding test_create_superuser

--- apps/customers/tests/test_model.py
+++ apps/customers/tests/test_model.py
@@ -8,3 +8,9 @@ class CustomerTestCase(TestCase):
         customer = Customer.objects.create_customer("test@test.com", "Test customer")
         self.assertEqual(customer.email, "test@test.com")
         self.assertEqual(customer.name, "Test customer")
+
+    def test_create_superuser(self):
+        customer = Customer.objects.create_superuser("super@test.com", "Test Super User", "123456")
+        self.assertEqual(customer.email, "super@test.com")
+        self.assertEqual(customer.name, "Test Super User")
+        self.assertEqual(customer.is_superuser, True)
