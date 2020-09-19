be73509df4c32639883babfd4f6678e1cd34ccd2
Gilson Paulino <gilsonbp@gmail.com>
2020-09-19 09:23:39 -0300

    Custom permission create action

--- apps/customers/permissions.py
+++ apps/customers/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class AllowCreate(permissions.BasePermission):
     def has_permission(self, request, view):
-        if view.action == "list":
+        if view.action == "create":
             return True
 
         return bool(request.user and request.user.is_authenticated)
