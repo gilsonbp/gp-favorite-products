9497e6617cc1788c03d00473156259d6f7f93f7c
Gilson Paulino <gilsonbp@gmail.com>
2020-09-19 12:10:43 -0300

    Adding API test_create_customer

---
+++ apps/customers/tests/test_api.py
@@ -0,0 +1,25 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+from apps.customers.models import Customer
+
+
+class CustomerAPITestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.customer = Customer.objects.create_customer("test@test.com", "Test Customer")
+
+    def setUp(self):
+        self.client = APIClient()
+        self.client.force_authenticate(self.customer)
+
+    def test_create_customer(self):
+        url = reverse("customers-list")
+        data = {
+            "email": "test1@test.com",
+            "password": "123456",
+            "name": "Test1 Customer",
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
