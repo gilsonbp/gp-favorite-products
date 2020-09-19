899d561e2382ee612b8604cd86e1f2b1b66e0fe4
Gilson Paulino <gilsonbp@gmail.com>
2020-09-19 09:32:46 -0300

    Customer urls

--- gpfavoriteproducts/urls.py
+++ gpfavoriteproducts/urls.py
@@ -1,4 +1,23 @@
-from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.urls import path, re_path
+from django.views import static as url_static
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
-urlpatterns = []
+from apps.customers.views import CustomerViewSet
+
+router = DefaultRouter()
+router.register("customers", CustomerViewSet, basename="customers")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("token/", obtain_auth_token),
+    re_path(r"^media/(?P<path>.*)$", url_static.serve, {"document_root": settings.MEDIA_ROOT},),
+    re_path(r"^static/(?P<path>.*)$", url_static.serve, {"document_root": settings.STATIC_ROOT},),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
