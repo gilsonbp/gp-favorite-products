d153878cd51184646a3159e2277cd2d03ad01eb2
Gilson Paulino <gilsonbp@gmail.com>
2020-09-19 20:53:03 -0300

    FavoriteProductSerializer

--- apps/favorites/serializers.py
+++ apps/favorites/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Product
+from .models import Product, FavoriteProduct
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -8,3 +8,10 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         read_only = ("id", "review_score")
         fields = ("id", "title", "brand", "price", "image", "review_score")
+
+
+class FavoriteProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FavoriteProduct
+        read_only = ("id", "customer")
+        fields = ("id", "customer", "product")
