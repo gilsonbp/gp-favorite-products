b03af20a0322abb0e42427f38b33d2ff8fb16a87
Gilson Paulino <gilsonbp@gmail.com>
2020-09-20 19:34:58 -0300

    Urls FavoriteProductViewSet

--- apps/favorites/urls.py
+++ apps/favorites/urls.py
@@ -1,13 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet
+from .views import ProductViewSet, FavoriteProductViewSet
 
 app_name = "favorites"
 
 
 router = DefaultRouter()
 router.register("products", ProductViewSet, basename="products")
+router.register("favorite-products", FavoriteProductViewSet, basename="favorite-products")
 
 urlpatterns = [
     path("", include(router.urls)),
