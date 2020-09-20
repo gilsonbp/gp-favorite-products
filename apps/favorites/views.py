71ff976c673bb7fe8d525ecb1a2640e51d764bf8
Gilson Paulino <gilsonbp@gmail.com>
2020-09-20 19:34:46 -0300

    Adding FavoriteProductViewSet

--- apps/favorites/views.py
+++ apps/favorites/views.py
@@ -1,9 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, FavoriteProduct
+from .serializers import ProductSerializer, FavoriteProductSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
+
+
+class FavoriteProductViewSet(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin):
+    serializer_class = FavoriteProductSerializer
+    queryset = FavoriteProduct.objects.all()
+
+    def get_queryset(self):
+        qs = super(FavoriteProductViewSet, self).get_queryset()
+        return qs.filter(customer=self.request.user)
