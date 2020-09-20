a15a33492f040cec7825efbf1f1c5744f81431ec
Gilson Paulino <gilsonbp@gmail.com>
2020-09-20 19:32:48 -0300

    FavoriteProductSerializer improvements

--- apps/favorites/serializers.py
+++ apps/favorites/serializers.py
@@ -11,7 +11,11 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class FavoriteProductSerializer(serializers.ModelSerializer):
+    customer = serializers.UUIDField(write_only=True, default=serializers.CurrentUserDefault())
+    title = serializers.CharField(read_only=True, source="product.title")
+    image = serializers.CharField(read_only=True, source="product.image.url")
+    price = serializers.DecimalField(read_only=True, source="product.price", max_digits=12, decimal_places=2)
+
     class Meta:
         model = FavoriteProduct
-        read_only = ("id", "customer")
-        fields = ("id", "customer", "product")
+        fields = ("customer", "product", "title", "image", "price")
